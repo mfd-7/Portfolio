b479e1103d3a96bd6857ad4b4cbd22aa261618f1
mfd-7 <muhtasimfuad3570@gmail.com>
2026-09-18 13:02:53 +0600

    Customize Django Admin panel headers

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Mr. MUHTASIM Admin Panel"
+admin.site.site_title = "MFD.7 Admin Portal"
+admin.site.index_title = "Welcome to Portfolio Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
